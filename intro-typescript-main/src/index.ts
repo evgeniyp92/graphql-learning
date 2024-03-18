5365966997f56123fa1da0be7bb3fbf6f26a3f00
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-18 08:51:08 +0100

    Add Apollo Server and GraphQL dependencies, implement Apollo Server and schema.graphql

--- intro-typescript-main/src/index.ts
+++ intro-typescript-main/src/index.ts
@@ -1,1 +1,22 @@
-// TODO
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import fs from 'fs';
+import path from 'path';
+import gql from 'graphql-tag'; // tagged template literal for wrapping GraphQL strings
+import { start } from 'repl';
+
+const typeDefs = gql(
+  fs.readFileSync(path.resolve(__dirname, './schema.graphql'), {
+    encoding: 'utf-8',
+  })
+);
+
+console.log(typeDefs);
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs: typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`🚀 Server ready at ${url}`);
+}
+
+startApolloServer();
