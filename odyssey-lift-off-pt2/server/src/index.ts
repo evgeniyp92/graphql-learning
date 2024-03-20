02becb92d032c263b7d3128ed75538ae8c7aaf8b
Evgeniy Pimenov <contact@evgeniypimenov.com>
2024-03-20 10:14:29 +0100

    rewrite index to use live data

--- odyssey-lift-off-pt2/server/src/index.ts
+++ odyssey-lift-off-pt2/server/src/index.ts
@@ -1,38 +1,29 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./schema";
-import { addMocksToSchema } from "@graphql-tools/mock";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-
-const mocks = {
-  Query: () => ({
-    tracksForHome: () => [...new Array(6)],
-  }),
-  Track: () => ({
-    id: () => "track_01",
-    title: () => "Astro Kitty, Space Explorer",
-    author: () => {
-      return {
-        name: "Grumpy Cat",
-        photo:
-          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
-      };
-    },
-    thumbnail: () =>
-      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
-    length: () => 1210,
-    modulesCount: () => 6,
-  }),
-};
+import { resolvers } from "./resolvers";
+import { TrackAPI } from "./track-api";
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    schema: addMocksToSchema({
-      schema: makeExecutableSchema({ typeDefs }),
-      mocks,
-    }),
+    // provide the type definitions and resolvers here
+    typeDefs,
+    resolvers,
   });
-  const { url } = await startStandaloneServer(server);
+  const { url } = await startStandaloneServer(server, {
+    context: async () => {
+      // Mount things here to make them available in contextValue
+      return {
+        dataSources: {
+          // can this maybe be replaced with a Singleton?
+          trackAPI: new TrackAPI({
+            // injecting the instance with RESTDataSource's cache manager
+            cache: server.cache,
+          }),
+        },
+      };
+    },
+  });
   console.log(`
     🚀  Server is running!
     📭  Query at ${url}
